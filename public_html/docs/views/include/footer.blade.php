b071a14a1a59ab26b2c1de3687b9f9798caff21e
Punawic <123066047+Punawic@users.noreply.github.com>
2023-01-21 12:59:05 +0700

    Delete public_html/docs directory

--- public_html/docs/views/include/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="www.zkads.com/" target="_blank">Zkads</a> {{ date('Y') }}</p>
-    </div>
-</div>
